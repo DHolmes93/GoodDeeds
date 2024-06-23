ebb3296e1ead6d3b8b1c104a45e0be7e63e0b109
Donovan Holmes <Donovanholmes17@gmail.com>
2024-06-23 10:04:52 -0400

    Initial Commit

---
+++ GoodDeeds/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoodDeeds
+//
+//  Created by Donovan Holmes on 6/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoodDeeds/GoodDeedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodDeedsApp.swift
+//  GoodDeeds
+//
+//  Created by Donovan Holmes on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct GoodDeedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
